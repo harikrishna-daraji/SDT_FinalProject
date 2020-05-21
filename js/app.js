5062a6bf41faaa0d482c1f7760d134a3b40eed67
Harikrishnadarji <65469425+harikrishna-daraji@users.noreply.github.com>
2020-05-20 21:02:07 -0400

    Successfully passed firebase data from one html to another file.

--- js/app.js
+++ js/app.js
@@ -20,8 +20,7 @@ const usersRef = dbRef.child('Products');
 const items = document.getElementById("items");
 
 
-
-	readUserData(); 
+	readUserData(); 	
 
 function readUserData() {
 	var imgCount = 1;
@@ -71,53 +70,36 @@ function readUserData() {
 	})
 }
 
+function setSelectedProductData() {
+
+}
+
 function readDataOfSelectedProduct(e) {
 	console.log("function called "+e.target.getAttribute("class"));
 	const productRef = dbRef.child('Products/'+e.target.getAttribute("class"));
 
-	var name = document.getElementById("name");
-	var price = document.getElementById("price");
-	var descrription = document.getElementById("description");
 
 	productRef.on("value",snap=>{
 		console.log("Entered in Snap");
 		snap.forEach(childSnap => {
 			console.log(childSnap.key+"-"+childSnap.val());
-			//name.innerHTML = name;
-			//price.innerHTML = price;
-			//description.innerHTML = description;
+			localStorage.setItem(childSnap.key,childSnap.val());
 		}); 
-	});
-}
-const addbutton=document.getElementById("button-submit");
-addbutton.addEventListener("click",added);
-function added(a){
-let productNumbers=localStorage.getItem('cartNumbers')
-console.log(productNumbers);
-console.log(typeof productNumbers);
-localStorage.setItem('cartNumbers',1);
-const productRef = dbRef.child('Products/'+a.target.getAttribute("class"));
-productRef.on("value",snap=>{
-		console.log("Entered in Snap");
-		snap.forEach(childSnap => {
-			console.log(childSnap.key+"-"+childSnap.val());
-});
+
 	});
 }
 
+/*function shopping_page(ai){
 
-
-/*function cartNumbers(){
-
-	productNumbers=parseInt(productNumbers);
-	if(productNumbers){localStorage.setItem('readDataOfSelectedProduct',productNumbers+1);}
-	else {localStorage.setItem('readDataOfSelectedProduct',1);}
-	return false;
-}*/
+	a = window.location.href="file:///C:/Users/vsn%20pooja/Documents/GitHub/SDT_FinalProject/shop.html";
+	
+	//var item-image = document.createElement("a"); 
+	//	item-image.setAttribute("href","img-id"+imgCount);
+}
 
 function Display(){
-	//var cart=document.getElementById("button-submit").innerHTML="ADDED";
-	//cart.href="cart.html";
-}   
+	var cart=document.getElementById("button-submit").innerHTML="ADDED";
+	//window.location.href="file:///C:/Users/vsn%20pooja/Documents/GitHub/SDT_FinalProject/cart.html";
+}   */
 
 
